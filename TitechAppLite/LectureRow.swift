2e52d23f390fa20741ba66cf4a125f344133386e
Yuki Takashima <y.takachy@gmail.com>
2020-09-26 16:59:27 +0900

    make space

--- TitechAppLite/LectureRow.swift
+++ TitechAppLite/LectureRow.swift
@@ -11,38 +11,38 @@ import SwiftUI
 struct LectureRow: View {
     
     var body: some View {
-        HStack {
-            Rectangle()
-                .fill(Color("main"))
-                .frame(width:5, height: 100)
-            VStack(alignment: .leading, spacing: 24) {
-                Text("08:50")
-                    .font(.system(size: 13))
-                    .foregroundColor(Color("textMain"))
-                    
-                Text("10:30")
-                    .font(.system(size: 13))
-                    .foregroundColor(Color("textSub"))
-            }
+            HStack {
+                Rectangle()
+                    .fill(Color("main"))
+                    .frame(width:5, height: 100)
+                VStack(alignment: .leading, spacing: 24) {
+                    Text("08:50")
+                        .font(.system(size: 13))
+                        .foregroundColor(Color("textMain"))
+                        
+                    Text("10:30")
+                        .font(.system(size: 13))
+                        .foregroundColor(Color("textSub"))
+                }
 
-            VStack(alignment: .leading, spacing: 24) {
-                Text("電磁気学aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                    .lineLimit(1)
-                    .font(.system(size: 15))
-                    .foregroundColor(Color("textMain"))
-                    
-                Text("Week1 Descriptionaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                    .lineLimit(1)
-                    .font(.system(size: 13))
-                    .foregroundColor(Color("textSub"))
-            }
-            Spacer()
-            Text("W222aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                .lineLimit(2)
-                .font(.system(size: 14))
-                .foregroundColor(Color("main"))
-                .frame(width:44)
-                .padding()
+                VStack(alignment: .leading, spacing: 24) {
+                    Text("電磁気学")
+                        .lineLimit(1)
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("textMain"))
+                        
+                    Text("Week1 Description")
+                        .lineLimit(1)
+                        .font(.system(size: 13))
+                        .foregroundColor(Color("textSub"))
+                }
+                Spacer()
+                Text("W222")
+                    .lineLimit(2)
+                    .font(.system(size: 14))
+                    .foregroundColor(Color("main"))
+                    .frame(width:44)
+                    .padding()
         }
     }
 }
